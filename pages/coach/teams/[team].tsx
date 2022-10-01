1eb2d6d336f69959366e3eab0b8d5b7ab03e6309
sg55kg <sdollar1@gmail.com>
2022-10-01 18:58:38 -0500

    Added mock data for formatting team page

--- pages/coach/teams/[team].tsx
+++ pages/coach/teams/[team].tsx
@@ -1,12 +1,52 @@
 import {NextPage} from "next";
 import CoachLayout from "../../../layouts/CoachLayout";
+import {Button} from "@chakra-ui/react";
 
+const mockAthletes = [
+    {
+        name: 'Jeff Smith',
+        age: 26,
+        email: 'email@email',
+        programs: [],
+        currentProgram: null
+    },
+    {
+        name: 'Angela Something',
+        age: 30,
+        email: 'email@email',
+        programs: [],
+        currentProgram: null
+    },
+    {
+        name: 'Tim Swords',
+        age: 19,
+        email: 'email@email',
+        programs: [],
+        currentProgram: null
+    },
+]
 
 const TeamPage: NextPage = () => {
 
+
     return (
         <CoachLayout>
-            Team Test
+            <div>
+                {mockAthletes.map((athlete, idx) => {
+                    return (
+                        <div key={idx}>
+                            <p>{athlete.name}</p>
+                            {athlete.currentProgram ?
+                                <p>Current Program</p> :
+                                <p>{`${athlete.name} does not have a program`}</p>
+                            }
+                            <Button>
+                                Start new program
+                            </Button>
+                        </div>
+                    )
+                })}
+            </div>
         </CoachLayout>
     )
 }
