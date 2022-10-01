8c3d7c7abe7309f38b5783704428b2d3de7b8afe
sg55kg <sdollar1@gmail.com>
2022-09-30 23:02:41 -0500

    Added skeleton for activity feed

--- components/coach/ActivityFeed.tsx
+++ components/coach/ActivityFeed.tsx
@@ -1,11 +1,29 @@
 import {FC} from "react";
+import {TabList, Tabs, Tab, TabPanels, TabPanel} from "@chakra-ui/tabs";
 
 
 const ActivityFeed: FC = () => {
 
     return (
         <div>
-
+            <Tabs>
+                <TabList>
+                    <Tab>
+                        Programs
+                    </Tab>
+                    <Tab>
+                        Activity
+                    </Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        3 athletes need a new program for next week
+                    </TabPanel>
+                    <TabPanel>
+                        Someone did something
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
         </div>
     )
 }
