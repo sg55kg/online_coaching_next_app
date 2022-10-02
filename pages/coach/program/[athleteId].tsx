4d2c179654cfbb27049125f001bbad2625a65b60
sg55kg <sdollar1@gmail.com>
2022-10-02 15:32:14 -0500

    Added currentAthlete to program/[athleteId]

--- pages/coach/program/[athleteId].tsx
+++ pages/coach/program/[athleteId].tsx
@@ -1,4 +1,9 @@
 import {NextPage} from "next";
+import {Button} from "@chakra-ui/react";
+import {useModal} from "../../../hooks/useModal";
+import NewProgramModal from "../../../components/coach/NewProgramModal";
+import {useCoachContext} from "../../../contexts/CoachContext";
+import {useRouter} from "next/router";
 
 const mockExercises = [
     {
@@ -26,9 +31,25 @@ const mockWeek = {
 
 const WriteProgramPage: NextPage = () => {
 
+    const router = useRouter()
+    const { athleteId } = router.query
+    const { viewModal, toggleModal } = useModal()
+    const { athletes } = useCoachContext()
+
+    const currentAthlete = athletes.find(a => a.id === athleteId)
+
     return (
         <div>
-            To do: UI for writing an athlete's program
+            <div>Current program overview here if it exists</div>
+            <div>
+                No Program, then:
+                <Button
+                    onClick={toggleModal}
+                >
+                    Create new Program
+                </Button>
+            </div>
+            <NewProgramModal isOpen={viewModal} onClose={toggleModal} athlete={currentAthlete} />
         </div>
     )
 }
